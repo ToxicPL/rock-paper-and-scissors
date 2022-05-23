63f66e3a7d7945c398a869e54c98abd2e1c6c3a9
Kacper Jedynak <36375892+ToxicPL@users.noreply.github.com>
2022-05-23 19:43:25 +0200

    Game

---
+++ game.py
@@ -0,0 +1,39 @@
+import random
+print("Welcome to the Rock, Papers and Scissors game")
+computer_wins = 0
+user_wins = 0
+
+options = ["rock", "paper", "scissors"]
+# rock: 0 Paper: 1 Scrissors: 2
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    computer_pick = options[random_number]
+
+    if user_input.lower == "rock" and computer_pick == "scissors":
+        print("You have won!")
+        user_wins += 1
+        continue
+
+    elif user_input.lower == "paper" and computer_pick == "rock":
+        print("You have won!")
+        user_wins += 1
+        continue
+
+    elif user_input.lower == "scissors" and computer_pick == "paper":
+        print("You have won!")
+        user_wins += 1
+        continue
+    else:
+        print("You have lost!")
+        computer_wins += 1
+
+print("--------------------Summary--------------------")
+print("Your score is {}".format(user_wins))
+print("Computer score is {}".format(computer_wins))
+print("--------------------Thanks for playing the game--------------------")
